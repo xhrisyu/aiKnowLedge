2d1c88cb44e1a0aeb86eb633f2aea915dc9a482b
xhris777 <chrisyuhimself@gmail.com>
2024-07-15 23:46:54 +0800

    update: multiple questions in one response

--- aiknowledge/llm/client.py
+++ aiknowledge/llm/client.py
@@ -63,13 +63,13 @@ class OpenAILLM:
             user_question: str,
             context: Optional[str],
             chat_history: list[dict],
-            temperature: Optional[float] = 0.6,
+            temperature: Optional[float] = 0.2,
             model_name: Optional[str] = None
     ) -> Generator[str, None, Optional[int]]:  # Generator[YieldType, SendType, ReturnType]
         messages = [
-            {"role": "system", "content": KNOWLEDGE_QA_PROMPT},
-            *chat_history,
-            {"role": "user", "content": f"{context}\n用户问题：{user_question}\n"}
+            # {"role": "system", "content": },
+            # *chat_history,
+            {"role": "user", "content": f"{KNOWLEDGE_QA_PROMPT}{context}\n用户问题：{user_question}\n"}
         ]
         response_stream = self._chat_completion.create(
             model=self.CHAT_MODEL if not model_name else model_name,
--- aiknowledge/webui/chatbot.py
+++ aiknowledge/webui/chatbot.py
@@ -58,7 +58,7 @@ def chatbot_page():
     # Sidebar (Settings)
     with st.sidebar:
         with st.expander("⚙️ 检索设置", expanded=True):
-            top_k = st.slider(label="Top K", min_value=1, max_value=10, value=4, step=1)
+            top_k = st.slider(label="Top K", min_value=1, max_value=10, value=6, step=1)
             # sim_threshold = st.slider(label="Similarity Threshold", min_value=0.0, max_value=1.0, value=0.50, step=0.01)
             # additional_context_length = st.slider(label=":orange[Additional Context Length]", min_value=0, max_value=300, value=50, step=5)
         with st.expander("⚙️ 模型设置", expanded=True):
@@ -180,7 +180,7 @@ def chatbot_page():
                         ###############################################
                         time1 = time.time()
                         qdrant_client.checkout_collection(QDRANT_COLLECTION_INTFLEX)
-                        embedding_response = llm_client.get_text_embedding(text=user_input)  # Embedding
+                        embedding_response = llm_client.get_text_embedding(text=user_query)  # Embedding
                         embedding_user_question = embedding_response.content
                         embedding_response_token_usage = embedding_response.token_usage
                         time2 = time.time()
@@ -251,11 +251,18 @@ def chatbot_page():
                         ###############################################
                         # Hybrid Search
                         ###############################################
-                        candidate_documents = st.session_state.vector_searched_documents[no_query] + st.session_state.keyword_searched_documents[no_query]
+                        # Remove the duplicate documents from vector_searched_documents and keyword_searched_documents
+                        # candidate_documents = st.session_state.vector_searched_documents[no_query] + st.session_state.keyword_searched_documents[no_query]
+                        candidate_documents = st.session_state.keyword_searched_documents[no_query]
+                        for vector_searched_candidate_docs in st.session_state.vector_searched_documents[no_query]:
+                            cur_chunk_id = vector_searched_candidate_docs["chunk_id"]
+                            if not any(doc["chunk_id"] == cur_chunk_id for doc in candidate_documents):
+                                candidate_documents.append(vector_searched_candidate_docs)
+
                         hybrid_searcher = get_hybrid_searcher()
                         hybrid_searcher.setup_reranking(rankings=candidate_documents, k=top_k)
                         st.session_state.hybrid_searched_documents.append(hybrid_searcher.get_cross_encoder_scores(
-                            query=user_input))
+                            query=user_query))
 
                         # Get original content from candidate_documents
                         for ranking_item in st.session_state.hybrid_searched_documents[no_query]:
@@ -311,57 +318,65 @@ def chatbot_page():
 
                         st.divider()
 
-                    ###############################################
-                    # Generate LLM Response
-                    ###############################################
-                    # Formatted context for prompt
-                    prompt_context = ""
+                ###############################################
+                # Generate LLM Response
+                ###############################################
+                # Formatted context for prompt
+                prompt_context = ""
+                prompt_context_chunk_ids = []  # !!!! multiple doc retrieve remove duplicate
+                for no_query in range(len(st.session_state.hybrid_searched_documents)):
                     for doc in st.session_state.hybrid_searched_documents[no_query]:
+                        chunk_id = doc["chunk_id"]
+                        if chunk_id in prompt_context_chunk_ids:
+                            continue
+                        else:
+                            prompt_context_chunk_ids.append(chunk_id)
+
                         doc_name = doc["doc_name"]
                         content = doc.get("content", "")
                         pre_content = doc.get("pre_content", "")
                         next_content = doc.get("next_content", "")
                         prompt_context += f"[文本来源]: 《{doc_name}》\n[正文]:{pre_content}{content}{next_content}\n<DIVIDER>"
 
-                    with st.spinner("AI思考中..."):
-                        # Get chat history from st.session.state
-                        # chat_history = st.session_state.messages
-                        # if len(chat_history) > chat_history_len:
-                        #     chat_history = chat_history[-chat_history_len:]
-                        chat_history = []
+                with st.spinner("AI思考中..."):
+                    # Get chat history from st.session.state
+                    # chat_history = st.session_state.messages
+                    # if len(chat_history) > chat_history_len:
+                    #     chat_history = chat_history[-chat_history_len:]
+                    chat_history = []
 
-                        print("Start to generate llm response...")
-                        time1 = time.time()
-                        if not is_stream:
-                            ai_response = llm_client.get_chat_response(
-                                user_question=user_input,
-                                context=prompt_context,
-                                chat_history=chat_history,
-                                temperature=temperature,
-                                model_name=chat_model_type
-                            )
-                            ai_response_token_usage = ai_response.token_usage
-                            st.markdown(ai_response.content)
-                        else:
-                            response_generator = llm_client.stream_chat_response(
-                                user_question=user_input,
-                                context=prompt_context,
-                                chat_history=chat_history,
-                                temperature=temperature,
-                                model_name=chat_model_type
-                            )
-                            ai_response = st.write_stream(response_generator)
-                            ai_response_token_usage = llm_client.stream_chat_token_usage
-                        time2 = time.time()
+                    print("Start to generate llm response...")
+                    time1 = time.time()
+                    if not is_stream:
+                        ai_response = llm_client.get_chat_response(
+                            user_question=user_input,
+                            context=prompt_context,
+                            chat_history=chat_history,
+                            temperature=temperature,
+                            model_name=chat_model_type
+                        )
+                        ai_response_token_usage = ai_response.token_usage
+                        st.markdown(ai_response.content)
+                    else:
+                        response_generator = llm_client.stream_chat_response(
+                            user_question=user_input,
+                            context=prompt_context,
+                            chat_history=chat_history,
+                            temperature=temperature,
+                            model_name=chat_model_type
+                        )
+                        ai_response = st.write_stream(response_generator)
+                        ai_response_token_usage = llm_client.stream_chat_token_usage
+                    time2 = time.time()
 
-                        print(f"Generate AI response time: {time2 - time1}\n")
-                        if token_and_time_cost_caption:
-                            chatbot_container.caption(f"llm response token usage: {ai_response_token_usage} | time cost: {time2 - time1}")
+                    print(f"Generate AI response time: {time2 - time1}\n")
+                    if token_and_time_cost_caption:
+                        chatbot_container.caption(f"llm response token usage: {ai_response_token_usage} | time cost: {time2 - time1}")
 
-                        if no_query != len(query_analysis_list) - 1:
-                            st.markdown("----------")
+                    if no_query != len(query_analysis_list) - 1:
+                        st.markdown("----------")
 
-                    st.session_state.messages.append({"role": "assistant", "content": ai_response})
+                st.session_state.messages.append({"role": "assistant", "content": ai_response})
 
     # Vertical divider
     # with gap:
